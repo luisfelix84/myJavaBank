0ec15aa70e5ab634ad7a3a952b1e30e07fdbb0f6
Luís Félix <luisfelix@MBP-de-Luis.Home>
2021-03-01 20:25:11 +0000

    new class promt and clqss messages

---
+++ src/org/academiadecodigo/javabank/PromptMenu.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.javabank;
+
+import org.academiadecodigo.bootcamp.Prompt;
+
+public class PromptMenu {
+
+    private Prompt prompt;
+
+    public void setPrompt(Prompt prompt) {
+        this.prompt = prompt;
+    }
+
+    public void show() {
+        
+    }
+
+}
---
+++ src/org/academiadecodigo/javabank/utils/Messages.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.javabank.utils;
+
+public class Messages {
+
+    public final static String COSTUMER_NUMBER = "PLEASE INSERT YOUR COSTUMER NUMBER: ";
+    public final static String ACCOUNT_NUMBER = "PLEASE INSERT YOUR ACCOUNT NUMBER: ";
+    public final static String WELCOME = "WELCOME TO JAVA BANK";
+    public final static String INSERT_AMOUNT = "PLEASE INSERT AMOUNT: ";
+    public final static String HAVE_ACCOUNTS = "YOU HAVE THE FOLLOWING ACCOUNTS: ";
+    public final static String TOTAL_BALANCE = "TOTAL BALANCE: ";
+
+
+}
